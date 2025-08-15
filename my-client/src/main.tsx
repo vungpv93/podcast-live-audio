4710dfce825d1d2f60ad6ab689b03ac3870a84f4
VungPV <vungpv93@gmail.com>
2025-08-15 11:41:15 +0700

    feat: Toast

--- my-client/src/main.tsx
+++ my-client/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Toaster
+      toastOptions={{
+        style: { color: '#713200', fontSize: 12, width: 256 },
+        position: 'bottom-right'
+      }}
+    />
   </StrictMode>,
 )
