8020ed3a659654206f4dacf1e07e8822a21f1d83
VungPV <vungpv93@gmail.com>
2025-09-09 10:02:13 +0700

    feat: disable db mysql

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,7 +8,8 @@ import { SignalingModule } from './signaling/signaling.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { redisStore } from 'cache-manager-redis-store';
 import * as process from 'node:process';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
+// import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -19,21 +20,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         store: await redisStore({ socket: { host: process.env.REDIS_HOST, port: 6379 }, ttl: 60 * 60 }),
       }),
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      synchronize: false,
-      logging: true,
-      migrationsRun: false,
-      autoLoadEntities: true,
-      poolSize: 100,
-      logger: 'file',
-      maxQueryExecutionTime: 3000,
-    }),
+    // TODO Need enable db mysql
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: process.env.DB_HOST,
+    //   port: Number(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   synchronize: false,
+    //   logging: true,
+    //   migrationsRun: false,
+    //   autoLoadEntities: true,
+    //   poolSize: 100,
+    //   logger: 'file',
+    //   maxQueryExecutionTime: 3000,
+    // }),
     HttpModule,
     MediasoupModule,
     SignalingModule,
