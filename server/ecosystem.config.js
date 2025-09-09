a1a8e64f6cd8b298ffb839aa41be77b33c6675f1
VungPV <vungpv93@gmail.com>
2025-09-09 10:21:07 +0700

    feat: ecosystem

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'live-app',
       script: 'dist/main.js',
-      instances: 'max',
+      instances: 1,
       exec_mode: 'cluster',
       watch: false,
       env: {
