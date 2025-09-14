c1da6e73364fea12502b07d95485c18aca95cddc
VungPV <vungpv93@gmail.com>
2025-09-14 14:12:46 +0700

    update

--- client/src/components/live-audio/index.tsx
+++ client/src/components/live-audio/index.tsx
@@ -1,28 +1,109 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import type { ILiveAudio } from '../../dto/live-audio';
 import AudioAnalyzer from '../AudioAnalyzer';
 import { useLiveAudio } from '../../hooks/useLiveAudio';
 import { useMic } from '../../hooks/useMic.ts';
 import { useLive } from '../../hooks/useLive.ts';
-import { HiStatusOnline } from 'react-icons/hi';
+import { HiStatusOffline, HiStatusOnline } from 'react-icons/hi';
 import { HiPlay } from 'react-icons/hi2';
 import { BsMicFill, BsMicMuteFill } from 'react-icons/bs';
 import Volume from '../Volume';
+import { useConfirm } from '../../hooks/useConfirm.tsx';
 
 const Index: React.FC<ILiveAudio> = ({ roomId, socket, auth }) => {
   console.log('auth is ', auth);
   const [volume, setVolume] = useState<number>(80);
   const { isMicOn, handleMic, localStream } = useMic();
-  const { liveData, handleLive } = useLive({ roomId, socket });
-
+  const { liveData, handleLive, closeLive } = useLive({ roomId, socket });
   const { audioStream, joinRoom } = useLiveAudio({
     roomId,
     socket,
     localStream,
   });
 
+  useEffect(() => {
+    console.log('Live data updated: ', liveData);
+  }, [liveData]);
+
+  const { confirm, ConfirmModal } = useConfirm();
+  const handleEndLive: () => Promise<void> = async (): Promise<void> => {
+    const ok = await confirm(
+      'Bạn có chắc muốn kết thúc phiên live podcast này không?',
+      {
+        title: 'Kết thúc phiên live podcast',
+      },
+    );
+
+    if (ok) {
+      console.log('Đang thực hiện kết thúc phiên live podcast...');
+      // await new Promise((res) => setTimeout(res, 3000));
+      await closeLive();
+      console.log('Phiên live podcast đã kết thúc ✅');
+    }
+  };
+
+  const handleStartLive = async () => {
+    const result = await confirm('Bạn có chắc chắn muốn bắt đầu phiên live?', {
+      title: 'Xác nhận bắt đầu',
+    });
+
+    if (result) {
+      await handleLive();
+    }
+  };
+
+  useEffect(() => {
+    console.log('DEBUG : Live Audio');
+    if (socket?.id && roomId && liveData.live)
+      joinRoom()
+        .then((response) => console.log(`AUTO JOIN_LIVE `, response))
+        .catch((err) => console.error('AUTO JOIN_LIVE:', err));
+  }, [socket, roomId, liveData.live, joinRoom]);
+
+  const isDisabled = useMemo(() => {
+    return !liveData?.entity?.status || liveData?.entity?.status === 'finished';
+  }, [liveData]);
+
   return (
     <section className="flex-[3] flex flex-col p-4 w-full">
+      {liveData?.entity?.status === 'ongoing' && (
+        <div
+          className="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800"
+          role="alert"
+        >
+          <svg
+            className="flex-shrink-0 inline w-4 h-4 me-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path d="M16.707 5.293a1 1 0 0 0-1.414 0L9 11.586 6.707 9.293a1 1 0 0 0-1.414 1.414l3 3a1 1 0 0 0 1.414 0l7-7a1 1 0 0 0 0-1.414Z" />
+          </svg>
+          <span className="sr-only"></span>
+          <div>Phiên live podcast đang được diễn ra.</div>
+        </div>
+      )}
+
+      {liveData?.entity?.status === 'finished' && (
+        <div
+          className="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+          role="alert"
+        >
+          <svg
+            className="flex-shrink-0 inline w-4 h-4 me-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm1 14.5H9v-2h2Zm0-4H9V5h2Z" />
+          </svg>
+          <span className="sr-only"></span>
+          <div>Phiên live podcast đã kết thúc.</div>
+        </div>
+      )}
+
       <div className="relative w-full flex justify-center">
         <AudioAnalyzer audioStream={localStream || audioStream || undefined} />
         <div className="absolute inset-0 flex items-center justify-center">
@@ -43,18 +124,18 @@ const Index: React.FC<ILiveAudio> = ({ roomId, socket, auth }) => {
         <div>
           {liveData && liveData?.live ? (
             <>
-              <button className="text-sm bg-red-600 hover:bg-red-700">
-                <HiStatusOnline />
-              </button>
-              &nbsp;&nbsp;
-              <button className="px-2 py-1 text-sm bg-red-600 rounded hover:bg-green-700">
+              <button
+                className="px-2 py-1 text-sm bg-red-600 rounded hover:bg-red-600 w-32"
+                onClick={handleEndLive}
+              >
                 Kết thúc
               </button>
             </>
           ) : (
             <button
-              className="px-2 py-1 text-sm bg-green-600 rounded hover:bg-green-700"
-              onClick={handleLive}
+              className={`px-2 py-1 text-sm bg-green-600 rounded hover:bg-green-700 w-32 ${isDisabled ? 'disabled:bg-gray-500 disabled:cursor-not-allowed disabled:hover:bg-gray-500' : ''}`}
+              onClick={handleStartLive}
+              disabled={isDisabled}
             >
               Bắt đầu live
             </button>
@@ -62,9 +143,6 @@ const Index: React.FC<ILiveAudio> = ({ roomId, socket, auth }) => {
         </div>
 
         <div className="flex space-x-2 align-center">
-          {/*<button className="px-2 py-1 text-sm bg-green-600 rounded hover:bg-green-700">*/}
-          {/*  <HiPause />*/}
-          {/*</button>*/}
           <button className="px-2 py-1 text-sm bg-green-600 rounded hover:bg-green-700">
             <HiPlay />
           </button>
@@ -72,13 +150,18 @@ const Index: React.FC<ILiveAudio> = ({ roomId, socket, auth }) => {
             <Volume value={volume} onChange={(v) => setVolume(v)} />
           </button>
           <button
-            className="px-2 py-1 text-sm bg-green-600 rounded"
-            onClick={joinRoom}
+            className={`text-sm bg-red-600 hover:bg-red-700 ${liveData && liveData?.live ? '' : 'disabled:bg-gray-500 disabled:cursor-not-allowed disabled:hover:bg-gray-500'}`}
+            disabled={true}
           >
-            Join live
+            {liveData && liveData?.live ? (
+              <HiStatusOnline />
+            ) : (
+              <HiStatusOffline />
+            )}
           </button>
         </div>
       </div>
+      <ConfirmModal />
     </section>
   );
 };
--- client/src/hooks/useMic.ts
+++ client/src/hooks/useMic.ts
@@ -1,38 +1,27 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import toast from 'react-hot-toast';
 
 interface IMic {
-  isMicOn: boolean,
-  localStream?: MediaStream,
-  handleMic: () => Promise<void>
+  isMicOn: boolean;
+  localStream?: MediaStream;
+  handleMic: () => Promise<void>;
 }
 
 export function useMic(): IMic {
   const [isMicOn, setIsMicOn] = useState<boolean>(false);
   const [localStream, setLocalStream] = useState<MediaStream>();
 
-  const handleMic = useCallback(async (): Promise<void> => {
-    if (isMicOn) {
-      setIsMicOn(false);
-      setLocalStream(undefined);
-      return;
-    }
-
+  const initMic: () => Promise<void> = useCallback(async () => {
     try {
-      if (localStream) {
-        const audioTracks: MediaStreamTrack[] = localStream.getAudioTracks();
-        if (audioTracks.length > 0) {
-          const newState: boolean = !audioTracks[ 0 ].enabled;
-          audioTracks.forEach((track: MediaStreamTrack) => ( track.enabled = newState ));
-          setIsMicOn(newState);
-        }
-        return;
-      }
-
-      const mediaStream: MediaStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+      const mediaStream: MediaStream =
+        await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: false,
+        });
       setLocalStream(mediaStream);
       setIsMicOn(true);
     } catch (e) {
+      setIsMicOn(false);
       let msg = 'Không thể truy cập MICRO.';
       if (e instanceof DOMException) {
         switch (e.name) {
@@ -43,7 +32,8 @@ export function useMic(): IMic {
             msg = 'Không tìm thấy thiết bị MICRO.';
             break;
           case 'NotReadableError':
-            msg = 'Không thể sử dụng MICRO (có thể đang bị ứng dụng khác chiếm).';
+            msg =
+              'Không thể sử dụng MICRO (có thể đang bị ứng dụng khác chiếm).';
             break;
           case 'SecurityError':
           case 'AbortError':
@@ -57,7 +47,27 @@ export function useMic(): IMic {
       }
       toast.error(msg);
     }
-  }, [isMicOn, localStream]);
+  }, []);
+
+  const handleMic: () => Promise<void> = useCallback(async () => {
+    if (!localStream) {
+      await initMic();
+      return;
+    }
+
+    const audioTracks: MediaStreamTrack[] = localStream.getAudioTracks();
+    if (audioTracks.length > 0) {
+      const newState: boolean = !audioTracks[0].enabled;
+      audioTracks.forEach((track) => (track.enabled = newState));
+      setIsMicOn(newState);
+    }
+  }, [localStream, initMic]);
+
+  useEffect(() => {
+    initMic()
+      .then((r) => console.log(r))
+      .catch((e) => console.error(e));
+  }, [initMic]);
 
   return { localStream, isMicOn, handleMic };
 }
