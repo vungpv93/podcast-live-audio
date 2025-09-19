c51f4cec4c1ab85e84dd147e634fe45178ea72dc
VungPV <vungpv93@gmail.com>
2025-09-19 11:31:16 +0700

    feat: upgrade

--- server/src/constants/ERRCD.enum.ts
+++ server/src/constants/ERRCD.enum.ts
@@ -3,7 +3,7 @@ export enum ERRCD {
   E100102 = 100102, // Phiên live không tồn tại
   E100103 = 100103, // Bạn không có quyền bắt đầu phiên live này
   E100104 = 100104, // Bạn không có quyền tham gia phiên live này
-  E100105 = 100105, // Thiếu tham số bắt buộc
+  E100105 = 100105, // Phiên live đã kết thúc rồi.
   E100106 = 100106, // Lỗi không xác định
   E100107 = 100107, // Thiết bị không hỗ trợ WebRTC
   E100108 = 100108, // Lỗi kết nối tới server media server
